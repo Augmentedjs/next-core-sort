32b13b74eb4365e7133971b358685c32dee137ac
thedocbwarren <jedi.master@augmentedjs.com>
2018-10-03 21:06:49 -0700

    first start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { sortObjects, mergeSort, quickSort, insertionSort, bubbleSort } from "./sort.js";
+
+module.exports.sortObjects = sortObjects;
+module.exports.mergeSort = mergeSort;
+module.exports.quickSort = quickSort;
+module.exports.insertionSort = insertionSort;
+module.exports.bubbleSort = bubbleSort;
